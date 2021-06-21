0d954b83b2ebd0e83ebdfcd5e588f538de150eb4
Jan <CacheMoneyPlaya1>
2021-06-21 22:54:37 +0100

    21/06/21

--- crypt_scan/test/Coinbase/test_coinbase_module.py
+++ crypt_scan/test/Coinbase/test_coinbase_module.py
@@ -1,43 +1,42 @@
 import sys
 sys.path.append('.')
 from exchanges.Coinbase import Coinbase
-import unittest
+import pytest
 
-class TestCoinbase(unittest.TestCase):
+class TestCoinbase():
 
-    TEST_MESSAGE = None
-    coi_mod = None
+    coi_module = None
+    coi_msg = None
+
 
     @classmethod
-    def setUpClass(cls):
+    def setup_class(cls):
         cls.coi_msg = {'type': 'l2update', 'product_id': 'BTC-USD', 'changes': [['buy', '40143.01', '0.26000000']], 'time': '2021-06-15T22:29:57.478518Z'}
-        cls.coi_mod = Coinbase(cls.coi_msg, None)
-        cls.coi_mod.isolate_order()
+        cls.coi_module = Coinbase(cls.coi_msg, None)
+        cls.coi_module.isolate_order()
 
 
     @classmethod
     def tearDownClass(cls):
-        cls.coi_mod = None
+        cls.coi_module = None
+        cls.coi_msg = None
+
 
     def test_coinbase_instance(self):
-        self.assertEqual(self.coi_mod.message, self.coi_msg)
+        assert self.coi_module.message == self.coi_msg
 
 
     def test_can_get_price(self):
-        self.assertEqual(self.coi_mod.get_price(), '40143.01')
+        assert self.coi_module.get_price() == '40143.01'
 
 
     def test_can_get_quantity(self):
-        self.assertEqual(self.coi_mod.get_quantity(), '0.26000000')
+        assert self.coi_module.get_quantity() == '0.26000000'
 
 
     def test_can_validate(self):
-        self.assertEqual(self.coi_mod.is_valid(), True)
+        assert self.coi_module.is_valid() == True
 
 
     def test_can_get_side(self):
-        self.assertEqual(self.coi_mod.get_side(), 'buy')
-
-
-if __name__ == '__main__':
-    unittest.main()
+        assert self.coi_module.get_side() == 'buy'
